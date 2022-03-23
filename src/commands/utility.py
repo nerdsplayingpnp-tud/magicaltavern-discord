3b781af29f66848cfe1da5481df35e5c87c2f722
bitfl0wer <fleeprivate@gmail.com>
2022-03-23 18:26:14 +0100

    Added check_admin-decorator Returns True if Context-User is Bot Owner, has Admin Perms or Admin Role defined in config.json

--- src/commands/utility.py
+++ src/commands/utility.py
@@ -1,3 +1,5 @@
+from operator import truediv
+from traceback import format_exception
 import discord
 from discord.ext import commands
 from logger.ownlogger import log
@@ -17,24 +19,27 @@ class UtilityCommands(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    def check_admin_role(ctx: commands.Context):
-        # TODO: This is not working! I just wanted to make a commit again
-        return ctx.message.author.id == id_role_admin
+    def check_admin():
+        def predicate(ctx):
+            return(commands.is_owner())
+        return commands.check(predicate)
 
-    @commands.command(name='latency')
+    @ commands.command(name='latency')
     async def ping(self, ctx: commands.Context):
         await ctx.send(f"Latenz: {round(self.bot.latency * 1000)}ms")
 
-    @commands.command(name='setstatus')
-    @commands.check(check_admin_role)
+    @ commands.command(name='setstatus')
+    @ commands.check_any(commands.has_role(id_role_admin), commands.has_permissions(administrator=True))
     async def setstatus(self, ctx: commands.Context, *, text: str):
         await self.bot.change_presence(activity=discord.Game(name=text))
 
-    @commands.command(name='debug')
+    @ commands.command(name='debug')
+    @ check_admin()
     async def debug(self, ctx: commands.Context):
-        print(id_role_admin)
+        # This is how we get IDs from the Context: ctx.message.author.roles[1].id
+        print('success')
+        # This setup-function is needed to let discord.py load the cog
 
 
-# This setup-function is needed to let discord.py load the cog
 def setup(bot: commands.Bot):
     bot.add_cog(UtilityCommands(bot))
