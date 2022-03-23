bb2835005c55ea7972c525f28d8b80f7419915c4
bitfl0wer <fleeprivate@gmail.com>
2022-03-23 18:38:44 +0100

    Replace unsafe path checks with from_project_root

--- src/bot.py
+++ src/bot.py
@@ -1,12 +1,13 @@
 from discord.ext import commands
 from logger.ownlogger import log
+from utils import from_project_root
 
 
 bot = commands.Bot(command_prefix="mt!")  # Create a Bot with Prefix 'mt!'
 
 # If a file token.txt exists: Run the bot. If not: Ask for the token to be stored in the file, and create the file.
 try:
-    with open('./config/token.txt') as token_file:
+    with open(from_project_root('/config/token.txt')) as token_file:
         __token__ = token_file.read()
 
         # Load all the cogs
@@ -18,7 +19,7 @@ except FileNotFoundError:
     log("Please enter your Bot Token. It will be stored in './config/token.txt', which is ignored by .gitignore.", 'red')
     # Getting the input as a variable BEFORE creating the file means, that the created file cannot be empty if the program aborts at this point.
     token = input()
-    tokenfile = open('./config/token.txt', 'w')
+    tokenfile = open(from_project_root('/config/token.txt'), 'w')
     tokenfile.write(token)
     tokenfile.close()
     log("Token stored. Please restart the bot.")
