538380a897cb4ccadd1b70d491b267b4c895d468
bitfl0wer <fleeprivate@gmail.com>
2022-03-24 13:37:39 +0100

    Fully linted

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,15 @@
-from discord.ext import commands
+"""
+    IMPORTS:
+
+    discord, discord.ext
+        Used for: Literally everything discord-api related.
+
+    logger.ownlogger: log
+        Used for: logging, duh.
+
+    utils
+        Used for: please see utils.py docstrings for an explanation,
+    """
 import discord
 from logger.ownlogger import log
 from utils import from_project_root
@@ -6,7 +17,8 @@ from utils import from_project_root
 
 bot = discord.Bot()  # Create a Bot with Prefix 'mt!'
 
-# If a file token.txt exists: Run the bot. If not: Ask for the token to be stored in the file, and create the file.
+# If a file token.txt exists: Run the bot. If not:
+# Ask for the token to be stored in the file, and create the file.
 try:
     with open(from_project_root('/config/token.txt'), encoding='utf-8') as token_file:
         __token__ = token_file.read()
@@ -17,7 +29,8 @@ try:
         bot.run(__token__)
 except FileNotFoundError:
 
-    log("Please enter your Bot Token. It will be stored in './config/token.txt', which is ignored by .gitignore.", 'red')
+    log("Please enter your Bot Token. It will be stored in" +
+        "'./config/token.txt', which is ignored by .gitignore.", 'red')
     # Getting the input as a variable BEFORE creating the file means,
     # that the created file cannot be empty if the program aborts at this point.
     token = input()
