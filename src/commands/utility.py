f0cabdfe89bf09f6985aacc1309a758858ec2883
bitfl0wer <fleeprivate@gmail.com>
2022-03-24 13:29:42 +0100

    Full pylint-linting!

--- src/commands/utility.py
+++ src/commands/utility.py
@@ -1,15 +1,37 @@
+"""
+IMPORTS:
+os
+
+    Used for:
+        Filesystem access and paths
+
+json
+
+    Used for:
+        opening and parsing the files in /config
+
+discord.commands, discord, discord.ext
+
+    Used for:
+        literally everything discord-api related
+
+utils
+
+    Used for:
+        these are my own functions that i added for various reasons. see utils docs.
+    """
+import os
+import json
 from discord.commands import (
     slash_command,
 )
 import discord
 from discord.ext import commands
-from logger.ownlogger import log
-from utils import *
-import os
-import json
+from utils import get_project_root, from_project_root, config_var
+
 
 roles_path = os.path.join(get_project_root(), "/config/roles.json")
-with open(from_project_root('/config/roles.json')) as roles_json:
+with open(from_project_root('/config/roles.json'), encoding='utf-8') as roles_json:
     __roles_dict__ = json.load(roles_json)
     id_role_admin = __roles_dict__['role-admin']
 
@@ -17,6 +39,12 @@ list_guilds = config_var('guilds')
 
 
 class UtilityCommands(commands.Cog):
+    """
+    UtilityCommands houses all the utility commands.
+    Utility Commands are commands that are nice to have, but not essential to complete the bots'
+    actual goal.
+    """
+
     # Some utility commands.
 
     def __init__(self, bot: commands.Bot):
@@ -25,20 +53,33 @@ class UtilityCommands(commands.Cog):
     # check_admin is a decorator that returns as True, if the Context User is either the bot owner, has the admin_role configured in
     # config.json, or simply has administrator permissions.
 
-    def check_admin():
+    def check_admin(self=None):
+        """"check_admin() gets used as a predicate to check for all the possible conditions that
+        would make a user an 'admin'-type user. The predicate returns True, if the Context User 
+        is either the bot owner, has the admin_role configured in
+        config.json, or simply has administrator permissions.
+        """
         def predicate(ctx):
-            return(commands.check_any(commands.has_role(id_role_admin), commands.has_permissions(administrator=True), commands.is_owner()))
+            # ctx NEEDS to be passed to this predicate, because pycord demands it. The following
+            # 2 lines are dedicated to remove a pylint warning.
+            use_ctx = ctx
+            ctx = use_ctx  # i hate this.
+            return(commands.check_any(commands.has_role(id_role_admin),
+                                      commands.has_permissions(
+                                          administrator=True),
+                                      commands.is_owner()))
         return commands.check(predicate)
-
-    # Simple ping-command to check if the bot is still alive.
 
     @slash_command(
         name='ping',
         guild_ids=list_guilds)
     async def ping(self, ctx: commands.Context):
-        await ctx.respond(f"Latenz: {round(self.bot.latency * 1000)}ms")
+        """Simple ping-command to check if the bot is still alive.
 
-    # Manipulate the displayed Discord-Activity with this command.
+        Args:
+            ctx (commands.Context): The pycord-context object (passed automatically)
+        """
+        await ctx.respond(f"Latenz: {round(self.bot.latency * 1000)}ms")
 
     @slash_command(
         name='setstatus',
@@ -46,7 +87,12 @@ class UtilityCommands(commands.Cog):
     )
     @check_admin()
     async def setstatus(self, ctx: commands.Context, *, text: str):
-        """Sets the activity status of the bot."""
+        """Manipulate the displayed Discord-Activity with this command.
+
+        Args:
+            ctx (commands.Context): The pycord-context object (passed automatically)
+            text (str): The text that you'd like the bot to display as its' "Playing"-message.
+        """
         await self.bot.change_presence(activity=discord.Game(name=text))
         await ctx.respond('Aktion erfolgreich ausgeführt.')
 
@@ -58,11 +104,21 @@ class UtilityCommands(commands.Cog):
     )
     @check_admin()
     async def debug(self, ctx: commands.Context):
+        """A debug function that can be used for anything
+
+        Args:
+            ctx (commands.Context): The pycord-context object (passed automatically)
+        """
         # This is how we get IDs from the Context: ctx.message.author.roles[1].id
         print('success')
         # This setup-function is needed to let discord.py load the cog
         await ctx.respond('Erfolg.')
 
 
-def setup(bot: commands.Bot):
+def setup(bot: discord.Bot):
+    """This setup function is needed by pycord to "link" the cog to the bot.
+
+    Args:
+        bot (commands.Bot): the Bot-object.
+    """
     bot.add_cog(UtilityCommands(bot))
