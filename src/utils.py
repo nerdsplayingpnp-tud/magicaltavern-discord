c565be6346e2c3eccf2b504f64df577fa4ecaad8
bitfl0wer <fleeprivate@gmail.com>
2022-03-24 13:44:35 +0100

    full linting

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+"""
+    I am not writing any more import-docstrings. these are pointless.
+    """
 from pathlib import Path
 import json
 
@@ -5,18 +8,40 @@ import json
 
 
 def get_project_root():
-    return Path(__file__).parent.parent
+    """Used to get the project folder path on any given harddrive. We do this to ensure that
+    filepaths are always relative, which means that this bot should be able to run anywhere.
 
-# from_project_root(path) is supposed to be a simple way to navigate directories relative to the project root.
-# Example: If your project root is '/home/user/this-project', calling 'from_project_root('/config/token.txt')'
-# returns '/home/user/this-project/config/token.txt'.
+    Returns:
+        Path: A Path-object
+    """
+    return Path(__file__).parent.parent
 
 
 def from_project_root(path: str) -> str:
+    """from_project_root(path) is supposed to be a simple way to navigate directories relative to 
+    the project root. Example: If your project root is '/home/user/this-project', calling
+    'from_project_root('/config/token.txt')' returns '/home/user/this-project/config/token.txt'.
+
+    Args:
+        path (str): path to travel to. always assumes you are in the projects' main dir, because
+        of get_project_root().
+
+    Returns:
+        str: an absolute path
+    """
     return str(str(get_project_root()) + path)
 
 
 def config_var(json_key: str):
-    with open(from_project_root('/config/config.json')) as conf:
+    """config_var is used to easily retrieve key/values from config.json without having to do the
+    full 'with open...' dance everytime.
+
+    Args:
+        json_key (str): the key which you want to retrieve the value from.
+
+    Returns:
+        any type: the value 
+    """
+    with open(from_project_root('/config/config.json'), encoding='utf-8') as conf:
         __conf_dict__ = json.load(conf)
         return __conf_dict__[json_key]
