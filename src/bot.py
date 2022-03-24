10095c1e2a9db33c5c215ab5fcc91e5f74ded8f0
bitfl0wer <fleeprivate@gmail.com>
2022-03-24 12:41:30 +0100

    Pylint-fixes

--- src/bot.py
+++ src/bot.py
@@ -8,7 +8,7 @@ bot = discord.Bot()  # Create a Bot with Prefix 'mt!'
 
 # If a file token.txt exists: Run the bot. If not: Ask for the token to be stored in the file, and create the file.
 try:
-    with open(from_project_root('/config/token.txt')) as token_file:
+    with open(from_project_root('/config/token.txt'), encoding='utf-8') as token_file:
         __token__ = token_file.read()
 
         # Load all the cogs
@@ -18,9 +18,11 @@ try:
 except FileNotFoundError:
 
     log("Please enter your Bot Token. It will be stored in './config/token.txt', which is ignored by .gitignore.", 'red')
-    # Getting the input as a variable BEFORE creating the file means, that the created file cannot be empty if the program aborts at this point.
+    # Getting the input as a variable BEFORE creating the file means,
+    # that the created file cannot be empty if the program aborts at this point.
     token = input()
-    tokenfile = open(from_project_root('/config/token.txt'), 'w')
+    tokenfile = open(from_project_root(
+        '/config/token.txt'), 'w', encoding='utf-8')
     tokenfile.write(token)
     tokenfile.close()
     log("Token stored. Please restart the bot.")
