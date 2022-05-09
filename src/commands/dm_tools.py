fc3cb2084f6e7d307c64122833c00c131a41b03f
bitfl0wer <fleeprivate@gmail.com>
2022-05-09 22:30:05 +0200

    Refined /suggest-campaign Now has way more parameters (all of the allowed 10), including: - Ruleset - Type (length) of campaign - Place - Time - Language - Character creation guidelines - Briefing - Notes - File attachment (image url)

--- src/commands/dm_tools.py
+++ src/commands/dm_tools.py
@@ -94,14 +94,17 @@ class DungeonMasterTools(commands.Cog):
     async def suggest_campaign(self,  # pylint: disable=R0913
                                ctx: discord.Interaction,
                                name: Option(str, "Der Name der Kampagne."),
-                               description: Option(str, description="Beschreibt hier kurz die Kampagne"),  # pylint: disable=C0301
+                               description: Option(str, description="Beschreibt hier kurz die Kampagne", name="beschreibung"),  # pylint: disable=C0301
                                min_players: Option(int, description="Wie viele Leute werden für die Kampagne mindestens benötigt? Min. 1, Max. 10", min_value=1, max_value=10),  # pylint: disable=C0301
                                max_players: Option(int, description="Wie viele Leute können maximal an der Kampagne teilnehmen? Min. 3, Max. 10", min_value=3, max_value=10),  # pylint: disable=C0301
-                               content_warnings: Option(str, description="Gib hier explizite Contenthinweise an."),
+                               place: Option(str, description="Wo wird deine Kampagne stattfinden?", choices=["Online", "Präsenz", "Beides"], name="ort"),
+                               time: Option(str, description="Zu welcher Zeit soll das PNP ungefähr stattfinden?", name="zeit"),
+                               content_warnings: Option(str, description="Gib hier explizite Contenthinweise an.", name="content_warnungen"),
                                ruleset: Option(str, description="Welches Regelwerk verwendet die Kampagne?", name="regelwerk"),
                                type: Option(str, name="typ", choices=["Oneshot (1-2 Sessions)", "Kürzere Kampagne (3-7 Sessions)", "Längere Kampagne (7+ Sessions)"], description="Ist deine Kampagne eher ein Oneshot oder eine längere Kampagne?"),
                                language: Option(str, name="sprache", description="In welchen Sprachen wird dein Abenteuer angeboten?", choices=["Englisch", "Deutsch", "Englisch & Deutsch"]),
                                character_creation: Option(str, name="charaktererstellung", description="Wie wird die Charaktererstellung gehandlet?"),
+                               briefing: Option(str, name="briefing", description="Wie hast du vor deine Spieler:innen zu briefen (Session 0 irl/online, per DM, selbstständig)?"),
                                notes: Option(str, "Hier ist Platz für alles, was noch offen ist.", name="notizen_und_sonstiges"),
                                image_url: Option(str, description="Direktlink zu einem Bild, welches du einbetten möchtest.", required=False) = None
                                ):
@@ -121,11 +124,14 @@ class DungeonMasterTools(commands.Cog):
         )
         embed.add_field(name="Minimal benötigte Anzahl an Spieler:innen", value=min_players)
         embed.add_field(name="Maximale Anzahl an Spieler:innen", value=max_players)
+        embed.add_field(name="Ort", value=place, inline=True),
+        embed.add_field(name="Zeit", value=time, inline=True),
         embed.add_field(name="Contentwarnungen", value=content_warnings, inline=False)
         embed.add_field(name="Verwendetes Regelwerk", value=ruleset, inline=True)
         embed.add_field(name="Länge der Kampagne", value=type, inline=True),
         embed.add_field(name="Sprache", value=language, inline=True),
         embed.add_field(name="Richtlinien zur Charaktererstellung", value=character_creation, inline=False),
+        embed.add_field(name="Briefing", value=briefing, inline=False),
         embed.add_field(name="Weitere Bemerkungen", value=notes, inline=False),
         embed.set_author(name=ctx.user.name)
         embed.set_image(url=image_url)
