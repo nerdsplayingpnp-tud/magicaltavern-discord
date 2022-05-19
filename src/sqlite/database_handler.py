1714c7dde733362c634e7d4c96e07c1f57fefc98
bitfl0wer <fleeprivate@gmail.com>
2022-05-19 17:20:40 +0200

    database_handler.py created - creates campaigns table on first run

---
+++ src/sqlite/database_handler.py
@@ -0,0 +1,21 @@
+import sqlite3
+import sqlite3 as db
+from src.helper_functions import from_project_root
+from src.logger.ownlogger import log
+
+conn = db.connect(from_project_root('/data/campaign.db'))
+cursor = conn.cursor()
+
+try:
+    cursor.execute("""CREATE TABLE campaigns (
+        name text,
+        minp integer,
+        maxp integer
+    )""")
+    conn.commit()
+    log('Table "campaigns" created.')
+except sqlite3.OperationalError:
+    log('Table "campaigns" already exists. Not creating table.')
+
+conn.commit()
+conn.close()
