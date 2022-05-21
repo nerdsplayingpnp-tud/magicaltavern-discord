54ee8468088c40f6d8524a5bc8213913ffc0f186
bitfl0wer <fleeprivate@gmail.com>
2022-05-21 16:17:40 +0200

    Add singleton for managed access to shared database

--- src/sqlite/database_handler.py
+++ src/sqlite/database_handler.py
@@ -2,18 +2,17 @@ import sqlite3
 from src.helper_functions import from_project_root
 from src.logger.ownlogger import log
 
-conn = sqlite3.connect(from_project_root('/data/campaign.db'))
-cursor = conn.cursor()
+
+class ConnectionMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
 
 
-def db_init() -> None:
-    try:
-        cursor.execute("""CREATE TABLE campaigns (
-            name text,
-            minp integer,
-            maxp integer
-        )""")
-        conn.commit()
-        log('Table "campaigns" created.')
-    except sqlite3.OperationalError:
-        log('Table "campaigns" already exists. Not creating table.')
+class Connection(metaclass=ConnectionMeta):
+    connection = sqlite3.connect(from_project_root('/data/campaign.db'))
+    cursor = connection.cursor()
