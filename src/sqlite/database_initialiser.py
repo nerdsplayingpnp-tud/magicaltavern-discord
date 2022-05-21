936114ebb8de100cb170deb1375b3e19e8a8a95c
bitfl0wer <fleeprivate@gmail.com>
2022-05-21 17:20:47 +0200

    Better db scheme

--- src/sqlite/database_initialiser.py
+++ src/sqlite/database_initialiser.py
@@ -2,19 +2,23 @@ import sqlite3
 from src.helper_functions import from_project_root
 from src.logger.ownlogger import log
 
-conn = sqlite3.connect(from_project_root('/data/campaign.db'))
-cursor = conn.cursor()
+conn_campaign = sqlite3.connect(from_project_root('/data/campaign.db'))
+cursor_campaign = conn_campaign.cursor()
 
 
 def db_init() -> None:
     try:
-        cursor.execute("""CREATE TABLE campaigns (
-            name text,
-            minp integer,
-            maxp integer
+        cursor_campaign.execute("""CREATE TABLE IF NOT EXISTS campaigns (
+            id integer PRIMARY KEY,
+            name text NOT NULL,
+            players_min integer NOT NULL,
+            players_max integer NOT NULL,
+            open integer NOT NULL DEFAULT 1,
+            date_created text NOT NULL
         )""")
-        conn.commit()
-        log('Table "campaigns" created.')
+        conn_campaign.commit()
+        conn_campaign.close()
+        log('Table "campaigns" exists.')
     except sqlite3.OperationalError:
         log('Table "campaigns" already exists. Not creating table.')
     return
