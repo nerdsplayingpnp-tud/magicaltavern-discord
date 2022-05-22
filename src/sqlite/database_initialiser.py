d30e12c745ffe9038e3585b1406626f7cf42155e
bitfl0wer <fleeprivate@gmail.com>
2022-05-22 16:31:17 +0200

    Changed DB Scheme

--- src/sqlite/database_initialiser.py
+++ src/sqlite/database_initialiser.py
@@ -1,22 +1,19 @@
 from src.sqlite.database_handler import Connection as Handler
 
-conn = Handler.create_connection()
-cursor = Handler.create_cursor(conn)  # should probably be done differently
-
 
 def db_init() -> None:
-    Handler.execute_query(Handler.create_connection(), """CREATE TABLE IF NOT EXISTS enrollments (
-        id integer PRIMARY KEY
+    # Handler.execute_query(Handler.create_connection(), """CREATE TABLE IF NOT EXISTS enrollments (
+    #     id integer PRIMARY KEY,
+    #     data blob
         
-    );""", True)
-    Handler.execute_query(Handler.create_connection(), """CREATE TABLE IF NOT EXISTS campaigns (
+    # );""", True)
+    Handler.create_table(Handler.create_connection(), """CREATE TABLE IF NOT EXISTS campaigns (
         id integer PRIMARY KEY,
         name text NOT NULL,
         players_min integer NOT NULL,
         players_max integer NOT NULL,
         open integer NOT NULL DEFAULT 1,
         date_created text NOT NULL,
-        enrollmentid INTEGER NOT NULL, 
-        FOREIGN KEY(enrollmentid) REFERENCES enrollments(id)
+        enrollments blob
     );""", True)
     return
